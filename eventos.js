dc783863b61498f45e70d9f3e855468685859adf
MatuteFE <126730585+MatuteFE@users.noreply.github.com>
2023-03-01 20:39:26 -0300

    initial commit

---
+++ eventos.js
@@ -0,0 +1,35 @@
+
+
+
+document.getElementById("DatosPers").onclick=function () {
+    document.getElementById("Mostrar").innerHTML= "Aqui coloco mis datos personales"
+}
+
+document.getElementById("FormacionAcad").onclick=function () {
+    document.getElementById("Mostrar").innerHTML= "Aqui coloco mi formacion academica"
+}
+
+document.getElementById("ExperienciaProf").onclick=function () {
+    document.getElementById("Mostrar").innerHTML= "Aqui coloco mi trayectoria profesional"
+}
+
+document.getElementById("Aspiraciones").onclick=function () {
+    document.getElementById("Mostrar").innerHTML= "Aqui expreso mis aspiraciones"
+}
+/**
+document.getElementById("DatosPers").onclick=function () {
+    document.getElementById("Misdatos").classList.toggle(active);
+}
+
+document.getElementById("FormacionAcad").onclick=function () {
+    document.getElementById("MisEstudios").style.display = block;
+}
+
+document.getElementById("ExperienciaProf").onclick=function () {
+    document.getElementById("Mitrayectoria").style.display = block;
+}
+
+document.getElementById("Aspiraciones").onclick=function () {
+    document.getElementById("Misaspiraciones").classList.toggle(mostrar);
+}
+ */
